889a0f6b2b9944086f24ce623b1d05fb2c87cfdc
Pierre-Marie <pmbroca@gmail.com>
2017-11-04 18:47:39 +0100

    correction pipe

--- src/app/custom.pipe.ts
+++ src/app/custom.pipe.ts
@@ -2,27 +2,27 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({ name: 'convertToHHmm' })
 export class ConvertToHHmmPipe implements PipeTransform {
-    transform(minutes: number): string {
-        let result = "";
-        result += Math.floor(minutes / 60) + " heures ";
-        result += minutes % 60 + " minutes";
-        return result;
-    }
+	transform(minutes: number): string {
+		let result = "";
+		result += Math.floor(minutes / 60) + " heures ";
+		result += minutes % 60 + " minutes";
+		return result;
+	}
 }
 
 @Pipe({ name: 'capitalizeWord' })
 export class CapitalizeWordPipe implements PipeTransform {
-    transform(str: string): string {
-    	str = str.replace(/([^\W_]+[^\s-]*) */g, function(s) {
-    		return s.charAt(0).toUpperCase() + s.substr(1).toLowerCase();
-    	});
-    	return str;
-    }
+	transform(str: string): string {
+		str = str.replace(/([^\W_]+[^\s-]*) */g, function(s) {
+			return s.charAt(0).toUpperCase() + s.substr(1).toLowerCase();
+		});
+		return str;
+	}
 }
 
 @Pipe({ name: 'filterCrew' })
 export class FilterCrewPipe implements PipeTransform {
 	transform(str: any[], args: any): string {
-        return str.filter(h => h.job.toLowerCase().indexOf(args.toLowerCase()) !== -1).map(s => s.name).join(', ');
+		return str.filter(h => h.job.toLowerCase() == args.toLowerCase()).map(s => s.name).join(', ');
 	}
 }
