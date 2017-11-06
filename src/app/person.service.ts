9566e34aff35c6950d8d8d0190c73d1b26646093
69pmb <pmbroca@gmail.com>
2017-11-06 12:03:41 +0100

    add movies of a person

--- src/app/person.service.ts
+++ src/app/person.service.ts
@@ -1,8 +1,10 @@
 import { Injectable }    from '@angular/core';
 import { Headers, Http, Response } from '@angular/http';
-
+import { Observable } from 'rxjs/Rx';
+import 'rxjs/add/observable/forkJoin';
 import 'rxjs/add/operator/toPromise';
 
+import { Movie } from './movie';
 import { Person } from './person';
 
 @Injectable()
@@ -15,6 +17,9 @@ export class PersonService {
     private videos = 'videos';
     private credits = 'credits';
     private recommendations = 'recommendations';
+    private original = "https://image.tmdb.org/t/p/original/";
+    private thumb = "https://image.tmdb.org/t/p/w154/";
+    private empty = './app/img/empty.jpg';
 
     constructor(private http: Http) { }
 
@@ -23,25 +28,45 @@ export class PersonService {
         return Promise.reject(error.message || error);
     }
     
+//    getPerson(id: number): Promise<Person> {
+//    	const url = `${this.personUrl}/${id}?${this.api_key}${this.langue}`;
+//    	return this.http.get(url)
+//    	.toPromise()
+//    	.then(response => this.mapPerson(response))
+//    	.catch(this.handleError);
+//    }
+    
     getPerson(id: number): Promise<Person> {
-    	const url = `${this.personUrl}/${id}?${this.api_key}${this.langue}`;
-    	return this.http.get(url)
-    	.toPromise()
-    	.then(response => this.mapPerson(response))
-    	.catch(this.handleError);
+        const url = `${this.personUrl}/${id}?${this.api_key}${this.langue}`;
+        const urlMovies = `${this.personUrl}/${id}/movie_credits?${this.api_key}${this.langue}`;
+        return Observable.forkJoin(
+            this.http.get(url).map((res: Response) => res.json()),
+            this.http.get(urlMovies).map((res: Response) => res.json())
+        ).map(responses => {
+            return [].concat(...responses);
+        }).map(response => this.mapPerson(response))
+          .catch(this.handleError);
     }
-   
-   mapPerson(response: Response): Person {
-	   let r = response.json();
-	   let profile_path = r.profile_path;
-	   let thumbnail = r.profile_path;
-	   if(profile_path === null) {
-		   profile_path = './app/img/empty.jpg';
-		   thumbnail = './app/img/empty.jpg';
-	   } else {
-		   profile_path = "https://image.tmdb.org/t/p/original/" + profile_path;
-		   thumbnail = "https://image.tmdb.org/t/p/w154/" + thumbnail;
-	   }
-	   return new Person(r.id, r.name, r.birthday, r.deathday, profile_path, thumbnail, r.biography, r.adult);
-   }
+
+    mapPerson(response: Response): Person {
+        let r = response[0];
+        let profile_path = r.profile_path;
+        let thumbnail = r.profile_path;
+        if (profile_path === null) {
+            profile_path = this.empty;
+            thumbnail = this.empty;
+        } else {
+            profile_path = this.original + profile_path;
+            thumbnail = this.thumb + thumbnail;
+        }
+
+        let cast = response[1].cast.slice(0, 6);
+        let moviesCast = cast.map((r: any) => <Movie>({
+            id: r.id, title: r.title, original_title: (r.original_title === r.title ? '' : r.original_title), date: r.release_date,
+            synopsis: r.overview, affiche: (r.poster_path === null ? this.empty : this.original + r.poster_path),
+            thumbnail: (r.poster_path === null ? this.empty : this.thumb + r.poster_path), adult: false, note: r.vote_average
+        }));
+
+        return new Person(r.id, r.name, r.birthday, r.deathday, profile_path, thumbnail, r.biography, r.adult, moviesCast);
+    }
 }
