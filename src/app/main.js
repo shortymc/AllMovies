6fec72206429a79bfcfabcd06d867f8ea9d82cdd
69pmb <pmbroca@gmail.com>
2017-11-06 12:10:08 +0100

    Delete main.js

--- src/app/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var core_1 = require("@angular/core");
-var platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
-var index_1 = require("./index");
-core_1.enableProdMode();
-platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(index_1.StarRatingModule);
-//# sourceMappingURL=main.js.map
