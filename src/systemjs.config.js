a5d4230d86e6746379843fffb68f4d2c0df93b16
69pmb <pmbroca@gmail.com>
2017-11-08 14:34:04 +0100

    Add files via upload

--- src/systemjs.config.js
+++ src/systemjs.config.js
@@ -26,6 +26,7 @@
 
       // other libraries
       'rxjs':                      'npm:rxjs',
+	  '@ng-bootstrap/ng-bootstrap': 'node_modules/@ng-bootstrap/ng-bootstrap/bundles/ng-bootstrap.js',
       'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js'
     },
     // packages tells the System loader how to load when no filename and/or no extension
