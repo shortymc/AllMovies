05a9703ff844ba48ae0782ccd340ab072b57305a
69pmb <pmbroca@gmail.com>
2017-11-09 15:55:49 +0100

    add crew details on person, panel images film

---
+++ app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    styleUrls: ['./app.component.css'],
+    template: `
+	<div class="header">
+		<h1>{{title}}</h1>
+	   <nav>
+		 <a routerLink="/dashboard">Dashboard</a>
+		 <a routerLink="/movies">Movies</a>
+	   </nav>
+	</div>
+   <router-outlet></router-outlet>
+    `
+})
+
+export class AppComponent {
+  title = 'Mon Appli';
+}
---
+++ movie.ts
@@ -0,0 +1,6 @@
+export class Movie {
+    constructor(public id: number, public title: string, public original_title: string, public date: string, public synopsis: string, public affiche: string, public thumbnail: string, 
+        public adult: boolean, public time: number, public note: number, public budget: number, public recette: number, public videos: string[], public actors: string[], 
+        public crew: string[], public recommendations: Movie[], public images: string[]) {
+    }
+}
