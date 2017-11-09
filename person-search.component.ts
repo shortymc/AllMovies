c264ea2537359bc0cc40cac81f53d30ed00372b8
69pmb <pmbroca@gmail.com>
2017-11-09 15:55:49 +0100

    add crew details on person, panel images film

---
+++ person-search.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import { Router }            from '@angular/router';
+
+import { Observable }        from 'rxjs/Observable';
+import { Subject }           from 'rxjs/Subject';
+
+// Observable class extensions
+import 'rxjs/add/observable/of';
+
+// Observable operators
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+
+import { PersonSearchService } from './person-search.service';
+import { Person } from './person';
+
+@Component({
+    selector: 'person-search',
+    templateUrl: './person-search.component.html',
+    styleUrls: ['./person-search.component.css'],
+    providers: [PersonSearchService]
+})
+export class PersonSearchComponent implements OnInit {
+    persons: Observable<Person[]>;
+    private searchTerms = new Subject<string>();
+    adult = false;
+
+    constructor(
+        private personSearchService: PersonSearchService,
+        private router: Router) { }
+
+    // Push a search term into the observable stream.
+    search(term: string): void {
+        this.searchTerms.next(term);
+    }
+
+    ngOnInit(): void {
+        this.persons = this.searchTerms
+            .debounceTime(300)        // wait 300ms after each keystroke before considering the term
+            //.distinctUntilChanged()   // ignore if next search term is same as previous
+            .switchMap(term => term   // switch to new observable each time the term changes
+                // return the http search observable
+                ? this.personSearchService.search(term, this.adult)
+                // or the observable of empty persons if there was no search term
+                : Observable.of<Person[]>([]))
+            .catch(error => {
+                // TODO: add real error handling
+                console.log(error);
+                return Observable.of<Person[]>([]);
+            });
+    }
+
+    gotoPerson(person: Person): void {
+        let link = ['/person', person.id];
+        this.router.navigate(link);
+    }
+}
