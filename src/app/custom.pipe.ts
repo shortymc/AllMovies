1cdc93065405891acb4cdcf9a62af79f8971031b
69pmb <pmbroca@gmail.com>
2017-12-05 17:14:46 +0100

    vo title and time in movies

--- src/app/custom.pipe.ts
+++ src/app/custom.pipe.ts
@@ -4,7 +4,7 @@ import * as moment from 'moment';
 @Pipe({ name: 'convertToHHmm' })
 export class ConvertToHHmmPipe implements PipeTransform {
 	transform(minutes: number): string {
-		let result = "";
+        let result = "";
 		result += Math.floor(minutes / 60) + " heures ";
 		result += minutes % 60 + " minutes";
 		return result;
