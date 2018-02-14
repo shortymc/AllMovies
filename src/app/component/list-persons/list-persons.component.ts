6427cfc36ccad143a961a4c2824ad4745c769f76
Pierre-Marie BROCA <PBR@dm-agpe.adds>
2018-02-14 15:03:52 +0100

    add ListPersonsComponent in movie detail

---
+++ src/app/component/list-persons/list-persons.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Person } from '../../model/person';
+import { Url } from '../../constant/url';
+
+@Component({
+  selector: 'app-list-persons',
+  templateUrl: './list-persons.component.html',
+  styleUrls: ['./list-persons.component.scss']
+})
+export class ListPersonsComponent implements OnInit {
+  @Input()
+  persons: Person;
+  @Input()
+  label: string;
+  Url = Url;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
