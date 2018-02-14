8c8534edf33e8c74ed8361755896dedf69fea9bd
Pierre-Marie BROCA <PBR@dm-agpe.adds>
2018-02-14 17:19:30 +0100

    format

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,18 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-root',
-    styleUrls: ['./app.component.scss'],
-    template: `
+  selector: 'app-root',
+  styleUrls: ['./app.component.scss'],
+  template: `
 	<div class="header">
-		<h1>{{title}}</h1>
-	   <nav>
-         <button class="btn btn-outline-primary" routerLink="/dashboard">Dashboard</button>
-		 <button class="btn btn-outline-primary" routerLink="/movies">Movies</button>
-		 <button class="btn btn-outline-primary" routerLink="/release">Sorties</button>
-	   </nav>
-	</div>
-   <router-outlet></router-outlet>
+	  <h1>{{title}}</h1>
+    <nav>
+      <button class="btn btn-outline-primary" routerLink="/dashboard">Dashboard</button>
+      <button class="btn btn-outline-primary" routerLink="/movies">Movies</button>
+      <button class="btn btn-outline-primary" routerLink="/release">Sorties</button>
+    </nav>
+  </div>
+  <router-outlet></router-outlet>
     `
 })
 
