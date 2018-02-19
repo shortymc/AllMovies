72d5c76e0ae11b5d14b7bcc60e52f3071f7aa4f9
Pierre-Marie BROCA <PBR@dm-agpe.adds>
2018-02-19 16:15:51 +0100

    move app template to file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  template: `
-	<div class="header">
-    <nav>
-      <button class="btn btn-outline-primary" routerLink="/dashboard">Dashboard</button>
-      <button class="btn btn-outline-primary" routerLink="/movies">Movies</button>
-      <button class="btn btn-outline-primary" routerLink="/release">Sorties</button>
-    </nav>
-  </div>
-  <router-outlet></router-outlet>
-    `
 })
 
 export class AppComponent {
