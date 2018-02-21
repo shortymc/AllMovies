4ed821066c245fb829ab54a42ae4baa6c4a5b119
Pierre-Marie BROCA <PBR@dm-agpe.adds>
2018-02-21 15:56:51 +0100

    move components to SharedModule and add movieDetailModule

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,43 @@
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { SubstractDatePipe, ConvertToHHmmPipe, CapitalizeWordPipe, FilterCrewPipe } from './custom.pipe';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { ListPersonsComponent } from './list-persons/list-persons.component';
+import { ListMoviesComponent } from './list-movies/list-movies.component';
+import { MetaComponent } from './meta/meta.component';
+
+
+@NgModule({
+  declarations: [
+    ConvertToHHmmPipe,
+    SubstractDatePipe,
+    CapitalizeWordPipe,
+    FilterCrewPipe,
+    ListMoviesComponent,
+    MetaComponent,
+    ListPersonsComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule,
+    NgbModule,
+  ],
+  exports: [
+    ConvertToHHmmPipe,
+    SubstractDatePipe,
+    CapitalizeWordPipe,
+    FilterCrewPipe,
+    ListMoviesComponent,
+    MetaComponent,
+    ListPersonsComponent
+  ],
+  providers: [
+  ]
+})
+
+export class SharedModule {
+  constructor() { }
+}
