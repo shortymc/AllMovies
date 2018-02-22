6b6e698d8449ca8283044291dafd8fb036215dd5
pmb <PMB@dm-agpe.adds>
2018-02-22 11:04:18 +0100

    movies et dashboard module + clean

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,10 @@
-import { SharedModule } from './shared/shared.module';
-import { CommonModule, DatePipe } from '@angular/common';
-import { NgModule, ModuleWithProviders, LOCALE_ID } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './component/dashboard/dashboard.component';
-import { MoviesComponent } from './component/movies/movies.component';
-import { MovieSearchComponent } from './component/movie-search/movie-search.component';
-import { PersonSearchComponent } from './component/person-search/person-search.component';
-import { MovieService } from './service/movie.service';
-import { DropboxService } from './service/dropbox.service';
-import { PersonService } from './service/person.service';
 import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http';
 import 'bootstrap';
 
@@ -24,18 +16,12 @@ import 'bootstrap';
     HttpClientModule,
     HttpClientJsonpModule,
     CommonModule,
-    AppRoutingModule,
-    SharedModule
+    AppRoutingModule
   ],
   declarations: [
     AppComponent,
-    DashboardComponent,
-    MoviesComponent,
-    MovieSearchComponent,
-    PersonSearchComponent
   ],
-  providers: [MovieService, PersonService, DatePipe, DropboxService,
-    { provide: LOCALE_ID, useValue: 'en' },],
+  providers: [{ provide: LOCALE_ID, useValue: 'en' }],
   bootstrap: [AppComponent]
 })
 
