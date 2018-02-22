1a389f19418623d784fabb9bc7f3e47809c90145
Pierre-Marie BROCA <PBR@dm-agpe.adds>
2018-02-22 11:04:00 +0100

    movies et dashboard module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './component/dashboard/dashboard.component';
-import { MoviesComponent } from './component/movies/movies.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'dashboard', component: DashboardComponent },
+  { path: 'dashboard', loadChildren: './application-modules/dashboard/dashboard.module#DashboardModule'},
   { path: 'movie', loadChildren: './application-modules/movie-detail/movie-detail.module#MovieDetailModule'},
   { path: 'person', loadChildren: './application-modules/person-detail/person-detail.module#PersonDetailModule'},
   { path: 'release', loadChildren: './application-modules/release/release.module#ReleaseModule'},
-  { path: 'movies', component: MoviesComponent }
+  { path: 'movies', loadChildren: './application-modules/movies/movies.module#MoviesModule' }
 ];
 
 @NgModule({
