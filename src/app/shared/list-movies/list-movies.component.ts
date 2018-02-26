f2a0974f90122111843548b509538620a5691e81
Pierre-Marie BROCA <PBR@dm-agpe.adds>
2018-02-26 15:47:44 +0100

    add pagination on movies-list

--- src/app/shared/list-movies/list-movies.component.ts
+++ src/app/shared/list-movies/list-movies.component.ts
@@ -12,14 +12,26 @@ export class ListMoviesComponent implements OnInit {
   movies: Movie[];
   @Input()
   label: string;
+  page = 1;
+  moviesToShow: Movie[];
+  pageSize = 5;
 
   constructor(private dropboxService: DropboxService) { }
 
   ngOnInit() {
+    this.getMoviesToShow(this.movies, this.page);
   }
 
   addList(): void {
     this.dropboxService.addMovieList(this.movies.filter((reco: Movie) => reco.checked), 'ex.json');
+  }
+
+  getMoviesToShow(movies, page) {
+    this.moviesToShow = movies.slice((page - 1) * this.pageSize, page * this.pageSize);
+  }
+
+  pageChanged(event: any) {
+    this.getMoviesToShow(this.movies, event);
   }
 
 }
