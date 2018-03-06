7f4a99d08d6046fc6d84b8b2ba4e74e506d6663c
Pierre-Marie BROCA <PBR@dm-agpe.adds>
2018-03-06 14:44:50 +0100

    add getProfilPath to utils class and use serviceUtils in every service

--- src/app/application-modules/dashboard/service/movie-search.service.ts
+++ src/app/application-modules/dashboard/service/movie-search.service.ts
@@ -1,41 +1,37 @@
+import { ServiceUtils } from './../../../service/serviceUtils';
+import { Utils } from './../../../shared/utils';
 import { Movie } from './../../../model/movie';
 import { Url } from './../../../constant/url';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class MovieSearchService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private serviceUtils: ServiceUtils, private http: HttpClient) { }
 
   search(term: string, adult: boolean, language: string): Observable<Movie[]> {
     let url = Url.MOVIE_SEARCH_URL + Url.API_KEY;
-    if (adult) { url += Url.ADULT_URL; }
+    if (adult) {
+      url += Url.ADULT_URL;
+    }
     url += `${Url.QUERY_URL}${term}${Url.LANGUE}${language}`;
     return this.http
-      .get(url, { headers: this.getHeaders() })
+      .get(url, { headers: this.serviceUtils.getHeaders() })
       .map(this.mapMovies)
       .map(data => data.slice(0, 5));
   }
 
-  getHeaders() {
-    const headers = new HttpHeaders();
-    headers.append('Accept', 'application/json');
-    return headers;
-  }
-
   mapMovies(response: any): Movie[] {
-    // The response of the API has a results
-    // property with the actual results
     // console.log(response.results);
     return response.results.map((r: any) => <Movie>({
       id: r.id,
       title: r.title,
       date: r.release_date,
       adult: r.adult,
-      original_title: r.original_title === r.title ? '' : r.original_title,
-      thumbnail: r.poster_path === null ? null : Url.IMAGE_URL_ORIGINAL + r.poster_path
+      original_title: Utils.getTitle(r),
+      thumbnail: Utils.getPosterPath(r, 0)
     }));
   }
 
--- src/app/service/person.service.ts
+++ src/app/service/person.service.ts
@@ -1,3 +1,4 @@
+import { Utils } from './../shared/utils';
 import { ServiceUtils } from './serviceUtils';
 import { Url } from './../constant/url';
 import { Injectable } from '@angular/core';
@@ -8,20 +9,20 @@ import { Person } from '../model/person';
 @Injectable()
 export class PersonService {
 
-  constructor(private utils: ServiceUtils) { }
+  constructor(private serviceUtils: ServiceUtils) { }
 
   getPerson(id: number, language: string): Promise<Person> {
     //        const url = `${Url.PERSON_URL}/${id}?${Url.API_KEY}${Url.LANGUE_FR},${Url.APPEND_IMAGES}`;
     const url = `${Url.PERSON_URL}/${id}?${Url.API_KEY}${Url.LANGUE}${language}${Url.APPEND}${Url.APPEND_IMAGES}`;
     const urlMovies = `${Url.PERSON_URL}/${id}/${Url.MOVIE_CREDITS_URL}?${Url.API_KEY}${Url.LANGUE}${language}`;
     return Observable.forkJoin(
-      this.utils.http.get(url),
-      this.utils.http.get(urlMovies)
+      this.serviceUtils.http.get(url),
+      this.serviceUtils.http.get(urlMovies)
     ).map(responses => {
       return [].concat(...responses);
     }).map(response => this.mapPerson(response))
       .toPromise()
-      .catch(this.utils.handleError);
+      .catch(this.serviceUtils.handleError);
   }
 
   mapPerson(response: any[]): Person {
@@ -29,24 +30,19 @@ export class PersonService {
     const resp = response[0];
     const crew = response[1].crew;
 
-    const asActor = response[1].cast.map((r: any) =>
-      this.toMovie(r, Url.IMAGE_URL_154, Url.IMAGE_URL_EMPTY, Url.IMAGE_URL_ORIGINAL));
-    const asDirector = crew.filter((r: any) => this.jobEquals(r.job, 'Director'))
-      .map((r: any) => this.toMovie(r, Url.IMAGE_URL_154, Url.IMAGE_URL_EMPTY, Url.IMAGE_URL_ORIGINAL));
-    const asProducer = crew.filter((r: any) => this.jobEquals(r.job, 'Producer'))
-      .map((r: any) => this.toMovie(r, Url.IMAGE_URL_154, Url.IMAGE_URL_EMPTY, Url.IMAGE_URL_ORIGINAL));
+    const asActor = response[1].cast.map((r: any) => this.toMovie(r));
+    const asDirector = crew.filter((r: any) => this.jobEquals(r.job, 'Director')).map((r: any) => this.toMovie(r));
+    const asProducer = crew.filter((r: any) => this.jobEquals(r.job, 'Producer')).map((r: any) => this.toMovie(r));
     const asCompositors = crew
       .filter((r: any) => (this.jobEquals(r.job, 'Compositors') || this.jobEquals(r.job, 'Original Music Composer')))
-      .map((r: any) => this.toMovie(r, Url.IMAGE_URL_154, Url.IMAGE_URL_EMPTY, Url.IMAGE_URL_ORIGINAL));
+      .map((r: any) => this.toMovie(r));
     const asScreenplay = crew.filter((r: any) => (this.jobEquals(r.job, 'Screenplay') || this.jobEquals(r.job, 'Writer')))
-      .map((r: any) => this.toMovie(r, Url.IMAGE_URL_154, Url.IMAGE_URL_EMPTY, Url.IMAGE_URL_ORIGINAL));
-    const asNovel = crew.filter((r: any) => this.jobEquals(r.job, 'Novel'))
-      .map((r: any) => this.toMovie(r, Url.IMAGE_URL_154, Url.IMAGE_URL_EMPTY, Url.IMAGE_URL_ORIGINAL));
+      .map((r: any) => this.toMovie(r));
+    const asNovel = crew.filter((r: any) => this.jobEquals(r.job, 'Novel')).map((r: any) => this.toMovie(r));
 
-    return new Person(resp.id, resp.name, resp.gender, resp.birthday, resp.deathday,
-      resp.profile_path === null ? Url.IMAGE_URL_EMPTY : Url.IMAGE_URL_ORIGINAL + resp.profile_path,
-      resp.profile_path === null ? Url.IMAGE_URL_EMPTY : Url.IMAGE_URL_154 + resp.profile_path, resp.biography,
-      resp.adult, resp.place_of_birth, resp.images.profiles.map((i: any) => i.file_path).filter((i: any) => i !== resp.profile_path),
+    return new Person(resp.id, resp.name, resp.gender, resp.birthday, resp.deathday, Utils.getProfilPath(resp, 0),
+      Utils.getProfilPath(resp, 154), resp.biography, resp.adult, resp.place_of_birth,
+      resp.images.profiles.map((i: any) => i.file_path).filter((i: any) => i !== resp.profile_path),
       asActor, asDirector, asProducer, asCompositors, asScreenplay, asNovel);
   }
 
@@ -54,11 +50,10 @@ export class PersonService {
     return job.toLowerCase() === filter.toLowerCase();
   }
 
-  toMovie(r: any, thumb: string, empty: string, original: string): any {
+  toMovie(r: any): any {
     return <Movie>({
-      id: r.id, title: r.title, original_title: (r.original_title === r.title ? '' : r.original_title), date: r.release_date,
-      synopsis: r.overview, affiche: (r.poster_path === null ? empty : original + r.poster_path),
-      thumbnail: (r.poster_path === null ? empty : thumb + r.poster_path), adult: false, note: r.vote_average
+      id: r.id, title: r.title, original_title: Utils.getTitle(r), date: r.release_date, synopsis: r.overview,
+      affiche: Utils.getPosterPath(r, 0), thumbnail: Utils.getPosterPath(r, 154), adult: false, note: r.vote_average
     });
   }
 }
