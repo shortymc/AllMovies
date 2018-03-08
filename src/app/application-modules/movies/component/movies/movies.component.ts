1638ac8e0a0c2f020e8ffbc7d8b31fffc7b9fc83
Pierre-Marie BROCA <PBR@dm-agpe.adds>
2018-03-08 15:58:58 +0100

    add paginator on movies and sorter on date

--- src/app/application-modules/movies/component/movies/movies.component.ts
+++ src/app/application-modules/movies/component/movies/movies.component.ts
@@ -1,13 +1,13 @@
-import { Subject } from 'rxjs/Subject';
-import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
+import { TranslateService } from '@ngx-translate/core';
 import { DropboxService } from './../../../../service/dropbox.service';
 import { Movie } from './../../../../model/movie';
 import { MovieService } from './../../../../service/movie.service';
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { forkJoin } from 'rxjs/observable/forkJoin';
-import { BreakpointObserver, MediaMatcher } from '@angular/cdk/layout';
+import { BreakpointObserver } from '@angular/cdk/layout';
 import { Sort } from '@angular/material/sort';
+import { PageEvent } from '@angular/material/paginator';
 
 const init_columns = ['id', 'thumbnail', 'title', 'original_title', 'date', 'note', 'language', 'genres', 'time'];
 
@@ -19,21 +19,20 @@ const init_columns = ['id', 'thumbnail', 'title', 'original_title', 'date', 'not
 export class MoviesComponent implements OnInit {
   displayedColumns = init_columns;
   movies: Movie[];
-  sortedData: Subject<Movie[]> = new Subject<Movie[]>();
-  public tableWidget: any;
+  sortedData: Movie[];
+  pageSize = 15;
+  pageIndex = 0;
+  pageSizeOptions = [5, 10, 25, 100];
+  pageEvent: PageEvent;
   constructor(private movieService: MovieService, private router: Router, private breakpointObserver: BreakpointObserver,
-    private dropboxService: DropboxService, private translate: TranslateService, private mediaMatcher: MediaMatcher) {
+    private dropboxService: DropboxService, private translate: TranslateService) {
   }
 
   ngOnInit(): void {
     this.breakpointObserver.observe([
       '(max-width: 700px)'
     ]).subscribe(result => {
-      if (result.matches) {
-        this.displayedColumns = ['thumbnail', 'title', 'date', 'language', 'time'];
-      } else {
-        this.displayedColumns = init_columns;
-      }
+      this.displayedColumns = result.matches ? ['thumbnail', 'title', 'date', 'language', 'time'] : init_columns;
     });
     this.getMovies();
   }
@@ -41,42 +40,64 @@ export class MoviesComponent implements OnInit {
   getMovies(): void {
     this.dropboxService.getAllMovies('ex.json').then(movies => {
       this.movies = movies;
-      this.sortedData.next(this.movies);
       this.checkAndFixData();
+      this.sortedData = this.movies.slice(0, this.pageSize);
     });
   }
 
-  sortData(sort: Sort) {
-    if (!sort.active || sort.direction == '') {
-      this.sortedData.next(this.movies);
-      return;
-    }
-    console.log('sort');
-    console.log(sort);
+  onPaginateChange(event: PageEvent) {
+    this.sortedData = this.movies.slice(event.pageIndex * event.pageSize, (event.pageIndex + 1) * event.pageSize);
+  }
 
-    this.sortedData.next(this.movies.sort((a, b) => {
-      let isAsc = sort.direction == 'asc';
-      // Date
-      // Durée
-      switch (sort.active) {
-        case 'id':
-          return this.compare(+a.id, +b.id, isAsc);
-        case 'title':
-          return this.compare(a.title, b.title, isAsc);
-        case 'original_title':
-          return this.compare(a.original_title, b.original_title, isAsc);
-        case 'note':
-          return this.compare(+a.note, +b.note, isAsc);
-        case 'language':
-          return this.compare(a.language, b.language, isAsc);
-        default:
-          return 0;
-      }
-    }));
+  sortData(sort: Sort) {
+    if (sort.active && sort.direction !== '') {
+      this.movies = this.movies.sort((a, b) => {
+        const isAsc = sort.direction === 'asc';
+        switch (sort.active) {
+          case 'id':
+            return this.compare(+a.id, +b.id, isAsc);
+          case 'title':
+            return this.compare(a.title, b.title, isAsc);
+          case 'original_title':
+            return this.compare(a.original_title, b.original_title, isAsc);
+          case 'note':
+            return this.compare(+a.note, +b.note, isAsc);
+          case 'language':
+            return this.compare(a.language, b.language, isAsc);
+          case 'date':
+            return this.compareDate(a.date, b.date, isAsc);
+          case 'time':
+            return this.compare(+a.time, +b.time, isAsc);
+          default:
+            return 0;
+        }
+      });
+    }
+    this.sortedData = this.movies.slice(0, this.pageEvent ? this.pageEvent.pageSize : this.pageSize);
   }
 
   compare(a, b, isAsc) {
     return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
+  }
+
+  compareDate(a, b, isAsc) {
+    const year1 = a.split('/')[0];
+    const month1 = a.split('/')[1];
+    const year2 = b.split('/')[0];
+    const month2 = b.split('/')[1];
+    let result;
+    if (year1 < year2) {
+      result = -1;
+    } else if (year1 > year2) {
+      result = 1;
+    } else {
+      if (month1 < month2) {
+        result = -1;
+      } else if (month1 > month2) {
+        result = 1;
+      }
+    }
+    return result * (isAsc ? 1 : -1);
   }
 
   checkAndFixData(): void {
