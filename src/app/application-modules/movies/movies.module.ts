1a7485b535be483f558e5bf5948018f4293abb6d
pmb <PMB@dm-agpe.adds>
2018-03-09 16:35:15 +0100

    add filter on genres in movies

--- src/app/application-modules/movies/movies.module.ts
+++ src/app/application-modules/movies/movies.module.ts
@@ -14,6 +14,7 @@ import { MatSortModule } from '@angular/material/sort';
 import { MatPaginatorModule, MatPaginatorIntl } from '@angular/material/paginator';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
 
 const childRoutes: Routes = [
   {
@@ -33,7 +34,8 @@ const childRoutes: Routes = [
     MatSortModule,
     MatPaginatorModule,
     MatInputModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatSelectModule
   ],
   providers: [
     MovieService,
