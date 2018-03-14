417049dcd665f4ca1543fd6e740936b4865f4c29
Pierre-Marie BROCA <PBR@dm-agpe.adds>
2018-03-14 11:59:37 +0100

    fix yarn and cordova calls

--- cordova.bat
+++ cordova.bat
@@ -1,10 +1,13 @@
 SET errorhandler=^|^| ^(PAUSE ^&^& EXIT /B 1^)
 
 cd C:\DEV\workspace\AllMovies %errorhandler%
-yarn cordova %errorhandler%
+
+call yarn cordova %errorhandler%
 RD /S /Q cordova\AllMovies\www %errorhandler%
 mkdir cordova\AllMovies\www %errorhandler%
 xcopy /s dist cordova\AllMovies\www %errorhandler%
 cd cordova\AllMovies %errorhandler%
-cordova build android %errorhandler%
+call cordova build android %errorhandler%
 copy /Y C:\DEV\workspace\AllMovies\cordova\AllMovies\platforms\android\app\build\outputs\apk\debug\app-debug.apk C:\Users\PBR\Dropbox %errorhandler%
+echo APK SUCCESSFULLY GENERATED
+pause>nul
