ef73da6ed729298a8df62498cd24586fcef9313c
Pierre-Marie BROCA <PBR@dm-agpe.adds>
2018-03-14 10:55:48 +0100

    add error handling

--- cordova.bat
+++ cordova.bat
@@ -1,8 +1,10 @@
-cd C:\DEV\workspace\AllMovies
-yarn cordova
-RD /S /Q cordova\AllMovies\www
-mkdir cordova\AllMovies\www
-xcopy /s dist cordova\AllMovies\www
-cd cordova\AllMovies
-cordova build android
-copy /Y C:\DEV\workspace\AllMovies\cordova\AllMovies\platforms\android\app\build\outputs\apk\debug\app-debug.apk C:\Users\PBR\Dropbox
+SET errorhandler=^|^| ^(PAUSE ^&^& EXIT /B 1^)
+
+cd C:\DEV\workspace\AllMovies %errorhandler%
+yarn cordova %errorhandler%
+RD /S /Q cordova\AllMovies\www %errorhandler%
+mkdir cordova\AllMovies\www %errorhandler%
+xcopy /s dist cordova\AllMovies\www %errorhandler%
+cd cordova\AllMovies %errorhandler%
+cordova build android %errorhandler%
+copy /Y C:\DEV\workspace\AllMovies\cordova\AllMovies\platforms\android\app\build\outputs\apk\debug\app-debug.apk C:\Users\PBR\Dropbox %errorhandler%
