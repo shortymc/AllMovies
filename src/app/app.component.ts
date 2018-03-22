c137f3d2289527555f50b9dc6459675fa34affc6
pmb <pm.broca@axess.fr>
2018-03-22 17:06:07 +0100

    add logout btn

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './service/auth.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
+
+  constructor(private auth: AuthService) { }
+
 }
