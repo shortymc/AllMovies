e3355b3429cfbbc9fda6fb10c2b115736b28fd38
pmb <pm.broca@axess.fr>
2018-03-22 14:14:42 +0100

    init connection page

--- src/app/application-modules/login/login.module.ts
+++ src/app/application-modules/login/login.module.ts
@@ -1,11 +1,31 @@
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LoginComponent } from './login.component';
+import { ConnectComponent } from './connect/connect.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { Routes, RouterModule } from '@angular/router';
+
+const childRoutes: Routes = [
+  {
+    path: '',
+    children: [
+      { path: '', redirectTo: 'connect', pathMatch: 'full' },
+      { path: 'connect', component: ConnectComponent },
+    ]
+  }];
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    RouterModule.forChild(childRoutes),
   ],
-  declarations: [LoginComponent]
+  declarations: [
+    ConnectComponent
+  ],
+  providers: []
 })
 export class LoginModule { }
