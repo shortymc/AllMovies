50bcd0321162b523fb293e9a4bc13cd170d4952b
pmb <pm.broca@axess.fr>
2018-03-22 13:39:53 +0100

    add Gard, AuthService and user handling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGard } from './app.gards';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'dashboard', loadChildren: './application-modules/dashboard/dashboard.module#DashboardModule'},
-  { path: 'movie', loadChildren: './application-modules/movie-detail/movie-detail.module#MovieDetailModule'},
-  { path: 'person', loadChildren: './application-modules/person-detail/person-detail.module#PersonDetailModule'},
-  { path: 'release', loadChildren: './application-modules/release/release.module#ReleaseModule'},
-  { path: 'movies', loadChildren: './application-modules/movies/movies.module#MoviesModule' }
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'dashboard', loadChildren: './application-modules/dashboard/dashboard.module#DashboardModule', canActivate: [AuthGard] },
+  { path: 'movie', loadChildren: './application-modules/movie-detail/movie-detail.module#MovieDetailModule', canActivate: [AuthGard] },
+  { path: 'person', loadChildren: './application-modules/person-detail/person-detail.module#PersonDetailModule', canActivate: [AuthGard] },
+  { path: 'release', loadChildren: './application-modules/release/release.module#ReleaseModule', canActivate: [AuthGard] },
+  { path: 'movies', loadChildren: './application-modules/movies/movies.module#MoviesModule', canActivate: [AuthGard] },
+  { path: 'login', loadChildren: './application-modules/login/login.module#LoginModule' },
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
---
+++ src/app/service/auth.service.ts
@@ -0,0 +1,78 @@
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/fromPromise';
+import { Url } from './../constant/url';
+import { DropboxService } from './dropbox.service';
+import { Injectable } from '@angular/core';
+import { User } from '../model/user';
+import * as jwtDecode from 'jwt-decode';
+
+@Injectable()
+export class AuthService {
+
+  redirectUrl: string;
+
+  constructor(private dropbox: DropboxService) { }
+
+  getToken(): string {
+    // console.log('getToken');
+    return localStorage.getItem('token');
+  }
+
+  isAuthenticated(): Promise<boolean> {
+    // console.log('isAuthenticated');
+    const token = this.getToken();
+    if (token) {
+      // console.log('true');
+      return new Promise((resolve) => { resolve(true) });
+    } else {
+      // console.log('false');
+      return new Promise((resolve) => { resolve(false) });
+    }
+  }
+
+  isAllowed(): Promise<boolean> {
+    // console.log('isAuthenticated');
+    const token = this.getToken();
+    if (token) {
+      // console.log('true');
+      return this.checkInfos(token);
+    } else {
+      // console.log('false');
+      return new Promise((resolve) => { resolve(false) });
+    }
+  }
+
+  checkInfos(token: string): Promise<boolean> {
+    // console.log('checkInfos');
+    const user_infos = <User>jwtDecode(token);
+    // console.log('token', user_infos);
+    return this.getUserInfo(user_infos.id).then((user: User) => {
+      // console.log('user', user);
+      return user.name === user_infos.name && user.password === user_infos.password;
+    });
+  }
+
+  getUserInfo(id: number): Promise<User> {
+    // console.log('getUserInfo', id);
+    return this.getUserFile().then((users: User[]) => {
+      // console.log('users', users);
+      return new Promise<User>((resolve, reject) => {
+        resolve(users.find((user: User) => user.id === id));
+      });
+    });
+  }
+
+  getUserFile(): Promise<User[]> {
+    // console.log('getUserFile');
+    return this.dropbox.downloadFile(Url.DROPBOX_USER_FILE).then(file =>
+      <User[]>JSON.parse(file)
+    ).catch((error: any) => {
+      console.error(error);
+      return new Promise((resolve, reject) => { });
+    });
+  }
+
+  logout() {
+
+  }
+}
