1f0d445924a4e3df4885eddb1714de34f8b2f4fd
pierre-marie broca <pierre-marie.broca@axess.fr>
2018-03-23 16:02:53 +0100

    I18n for register and required validation

--- src/app/application-modules/login/register/register.component.ts
+++ src/app/application-modules/login/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../../service/auth.service';
 import * as crypto from 'crypto-js';
 import { User } from '../../../model/user';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-register',
@@ -14,7 +15,7 @@ export class RegisterComponent implements OnInit {
   question: string;
   answer: string;
 
-  constructor(private auth: AuthService) { }
+  constructor(private auth: AuthService, private translate: TranslateService) { }
 
   ngOnInit() {
   }
