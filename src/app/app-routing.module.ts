9ee9611c7122cf648e00d8507d414e9740179f07
pmb <pm.broca@axess.fr>
2018-04-05 15:51:33 +0200

    add discover module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ const routes: Routes = [
   { path: 'person', loadChildren: './application-modules/person-detail/person-detail.module#PersonDetailModule', canActivate: [AuthGard] },
   { path: 'release', loadChildren: './application-modules/release/release.module#ReleaseModule', canActivate: [AuthGard] },
   { path: 'movies', loadChildren: './application-modules/movies/movies.module#MoviesModule', canActivate: [AuthGard] },
+  { path: 'discover', loadChildren: './application-modules/discover/discover.module#DiscoverModule', canActivate: [AuthGard] },
   { path: 'login', loadChildren: './application-modules/login/login.module#LoginModule' },
   { path: '**', redirectTo: '' }
 ];
