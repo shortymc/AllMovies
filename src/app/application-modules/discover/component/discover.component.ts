0c12f0ed4adb5922807bc30370b0751a3fd5d5b0
pierre-marie broca <pierre-marie.broca@axess.fr>
2018-04-06 14:06:20 +0200

    add selection on discover

--- src/app/application-modules/discover/component/discover.component.ts
+++ src/app/application-modules/discover/component/discover.component.ts
@@ -3,6 +3,7 @@ import { Discover } from './../../../model/discover';
 import { TranslateService } from '@ngx-translate/core';
 import { MovieService } from './../../../service/movie.service';
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-discover',
@@ -15,8 +16,9 @@ export class DiscoverComponent implements OnInit {
   sortChoices: string[];
   sortChosen: string;
   page: PageEvent;
+  nbChecked = 0;
 
-  constructor(private movieService: MovieService, private translate: TranslateService) { }
+  constructor(private movieService: MovieService, private translate: TranslateService, private router: Router) { }
 
   ngOnInit() {
     this.sortDir.value = 'desc';
@@ -29,8 +31,23 @@ export class DiscoverComponent implements OnInit {
     console.log('this.sortDir.value', this.sortDir.value);
     if (initPagination || !this.page) {
       this.page = new PageEvent();
+      this.nbChecked = 0;
     }
-    this.movieService.getMoviesDiscover(this.translate.currentLang, this.sortChosen, this.sortDir.value, this.page.pageIndex + 1).then(result => this.discover = result);
+    this.movieService.getMoviesDiscover(this.translate.currentLang, this.sortChosen, this.sortDir.value, this.page.pageIndex + 1)
+      .then(result => this.discover = result);
+  }
+
+  gotoDetail(id: number, event): void {
+    const key = event.which;
+    if (key === 1 && event.type !== 'mousedown') {
+      this.router.navigate(['movie', id]);
+    } else if (key === 2) {
+      window.open('/movie/' + id);
+    }
+  }
+
+  updateSize() {
+    this.nbChecked = this.discover.movies.filter(movie => movie.checked).length;
   }
 
 }
