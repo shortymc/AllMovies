3f89b24eaf1eaacd5eeeba75e4f90d7704655923
pierre-marie broca <pierre-marie.broca@axess.fr>
2018-10-26 11:41:25 +0200

    add variables in deployment script

--- cordova.ps1
+++ cordova.ps1
@@ -1,8 +1,13 @@
-cd C:\DEV\workspace\AllMovies
+Set-Variable -Name "workspace" -Value "C:\DEV\workspace\AllMovies"
+$dropbox = $workspace + "\src\app\shared\service\dropbox.service.ts"
+$apkDir = $workspace + "\cordova\AllMovies\platforms\android\app\build\outputs\apk\debug\app-debug.apk"
+Set-Variable -Name "outputDir" -Value "C:\Users\PBR\Dropbox\Documents\Dev Perso"
 
-$content = Get-Content("C:\DEV\workspace\AllMovies\src\app\service\dropbox.service.ts") | Out-String
+cd $workspace
+
+$content = Get-Content($dropbox) | Out-String
 $content = $content.replace("import Dropbox = require('dropbox');","import Dropbox from 'dropbox';")
-$content | out-file "C:\DEV\workspace\AllMovies\src\app\service\dropbox.service.ts"
+$content | out-file $dropbox
 
 yarn cordova
 rm -r -fo cordova\AllMovies\www
@@ -10,11 +15,11 @@ mkdir cordova\AllMovies\www
 xcopy /s dist cordova\AllMovies\www
 cd cordova\AllMovies
 cordova build android
-Copy-Item "C:\DEV\workspace\AllMovies\cordova\AllMovies\platforms\android\app\build\outputs\apk\debug\app-debug.apk" -Destination "C:\Users\PBR\Dropbox\Documents\Dev Perso" -force
+Copy-Item $apkDir -Destination $outputDir -force
 
-git checkout HEAD -- C:\DEV\workspace\AllMovies\src\app\service\dropbox.service.ts
+git checkout HEAD -- $dropbox
 
-if((Get-Item C:\Users\PBR\Dropbox\app-debug.apk).length -lt 5MB) {
+if((Get-Item ($outputDir + "\app-debug.apk")).length -lt 5MB) {
 	Write-Host "AN ERROR OCCURRED" -ForegroundColor Red
 } else {
 	Write-Host "APK SUCCESSFULLY GENERATED" -ForegroundColor Green
