938bf08741ecd2cb5845d99876e723249f8a7286
pierre-marie broca <pierre-marie.broca@axess.fr>
2018-10-26 11:03:31 +0200

    dispatch constants on different files

---
+++ src/app/constant/release-type.ts
@@ -0,0 +1,8 @@
+export class ReleaseType {
+  static readonly RELEASE_PREMIERE = '1';
+  static readonly RELEASE_THEATRICAL_LIMITED = '2';
+  static readonly RELEASE_THEATRICAL = '3';
+  static readonly RELEASE_DIGITAL = '4';
+  static readonly RELEASE_PHYSICAL = '5';
+  static readonly RELEASE_TV = '6';
+}
