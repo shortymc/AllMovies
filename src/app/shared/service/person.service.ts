8e49aa8000f6175b1186706134f71a6be3b5fb36
pierre-marie broca <pierre-marie.broca@axess.fr>
2018-10-26 10:51:23 +0200

    add personUrlBuilder

--- src/app/shared/service/person.service.ts
+++ src/app/shared/service/person.service.ts
@@ -6,14 +6,18 @@ import { UtilsService } from './utils.service';
 import { Url } from './../../constant/url';
 import { Person } from '../../model/person';
 import { ToastService } from './toast.service';
+import { UrlBuilder } from '../urlBuilder';
 
 @Injectable()
 export class PersonService {
 
-  constructor(private serviceUtils: UtilsService, private toast: ToastService) { }
+  constructor(
+    private serviceUtils: UtilsService,
+    private toast: ToastService
+  ) { }
 
   getPerson(id: number, language: string): Promise<Person> {
-    const url = `${Url.PERSON_URL}/${id}?${Url.API_KEY}${Url.LANGUE}${language}${Url.APPEND}${Url.APPEND_IMAGES}${Url.INCLUDE_IMAGE_LANGUAGE}${language},null`;
+    const url = UrlBuilder.personUrlBuilder(id, language, true);
     const urlMovies = `${Url.PERSON_URL}/${id}/${Url.MOVIE_CREDITS_URL}?${Url.API_KEY}${Url.LANGUE}${language}`;
     return Observable.forkJoin(
       this.serviceUtils.getObservable(url),
