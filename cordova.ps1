895d6bc57a99ebeb8151fdf7624a4413c4d4380f
pierre-marie broca <pierre-marie.broca@axess.fr>
2018-10-30 11:11:44 +0100

    add timestamp to apk release

--- cordova.ps1
+++ cordova.ps1
@@ -1,7 +1,7 @@
 $dir = Get-Location
 Set-Variable -Name "workspace" -Value "C:\DEV\workspace\AllMovies"
 $dropbox = $workspace + "\src\app\shared\service\dropbox.service.ts"
-$apkDir = $workspace + "\cordova\AllMovies\platforms\android\app\build\outputs\apk\debug\app-debug.apk"
+$apkDir = $workspace + "\cordova\AllMovies\platforms\android\app\build\outputs\apk\debug"
 Set-Variable -Name "outputDir" -Value "C:\Users\PBR\Dropbox\Documents\Dev Perso"
 
 cd $workspace
@@ -16,11 +16,14 @@ mkdir cordova\AllMovies\www
 xcopy /s dist cordova\AllMovies\www
 cd cordova\AllMovies
 cordova build android
-Copy-Item $apkDir -Destination $outputDir -force
+
+$newName = "app-debug_" + (Get-Date -Format FileDateTime) + ".apk"
+Rename-Item -Path ($apkDir + "\app-debug.apk") -NewName $newName
+Copy-Item ($apkDir + "\" + $newName) -Destination $outputDir -force
 
 git checkout HEAD -- $dropbox
 
-if((Get-Item ($outputDir + "\app-debug.apk")).length -lt 5MB) {
+if((Get-Item ($outputDir + "\" + $newName)).length -lt 5MB) {
 	Write-Host "AN ERROR OCCURRED" -ForegroundColor Red
 } else {
 	Write-Host "APK SUCCESSFULLY GENERATED" -ForegroundColor Green
