8ccaaae0395e29d5f6faff7a1bc912d4f1aeed74
pmb <pm.broca@axess.fr>
2018-10-30 16:56:54 +0100

    finish migration font awesome

--- src/app/constant/duck-duck-go.ts
+++ src/app/constant/duck-duck-go.ts
@@ -1,8 +1,11 @@
+import { faDatabase, faFilm } from '@fortawesome/free-solid-svg-icons';
+import { faImdb, faWikipediaW } from '@fortawesome/free-brands-svg-icons';
+
 export class DuckDuckGo {
-  static readonly SEARCH_BANG_METACRITIC = { site: 'metacritic', icon: 'fa-database' };
-  static readonly SEARCH_BANG_SENSCRITIQUE = { site: 'scq', icon: 'fa-film' };
-  static readonly SEARCH_BANG_IMDB = { site: 'imdb', icon: 'fa-imdb' };
-  static readonly SEARCH_BANG_WIKI_EN = { site: 'wen', icon: 'fa-wikipedia-w' };
-  static readonly SEARCH_BANG_WIKI_FR = { site: 'wikifr', icon: 'fa-wikipedia-w' };
+  static readonly SEARCH_BANG_METACRITIC = { site: 'metacritic', icon: faDatabase };
+  static readonly SEARCH_BANG_SENSCRITIQUE = { site: 'scq', icon: faFilm };
+  static readonly SEARCH_BANG_IMDB = { site: 'imdb', icon: faImdb };
+  static readonly SEARCH_BANG_WIKI_EN = { site: 'wen', icon: faWikipediaW };
+  static readonly SEARCH_BANG_WIKI_FR = { site: 'wikifr', icon: faWikipediaW };
   static readonly DUCKDUCKGO_URL = 'https://api.duckduckgo.com/?q=!';
 }
