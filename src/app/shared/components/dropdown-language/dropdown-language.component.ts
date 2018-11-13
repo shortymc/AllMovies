16259659c302d2d8cfb77e382608e61fabc770e2
pierre-marie broca <pierre-marie.broca@axess.fr>
2018-11-13 11:27:02 +0100

    add lang to user on register and header

--- src/app/shared/components/dropdown-language/dropdown-language.component.ts
+++ src/app/shared/components/dropdown-language/dropdown-language.component.ts
@@ -1,5 +1,10 @@
 import { TranslateService } from '@ngx-translate/core';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { distinctUntilChanged } from 'rxjs/operators';
+
+import { LangService } from './../../service/lang.service';
+import { AuthService } from './../../service/auth.service';
+import { Lang } from '../../../model/model';
 
 @Component({
   selector: 'app-dropdown-language',
@@ -7,17 +12,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dropdown-language.component.scss']
 })
 export class DropdownLanguageComponent implements OnInit {
-  language: string;
+  @Input()
+  userLang: boolean;
+  @Output()
+  lang = new EventEmitter<Lang>();
+  language: Lang;
+  langList: Lang[];
 
-  constructor(private translate: TranslateService) { }
+  constructor(
+    private translate: TranslateService,
+    private langService: LangService,
+    private auth: AuthService
+  ) { }
 
   ngOnInit(): void {
-    this.language = this.translate.getBrowserLang();
+    this.langService.getAll().then(langs => {
+      this.langList = langs;
+      if (this.userLang) {
+        this.auth.isLogged.pipe(distinctUntilChanged()).subscribe(isLogged => {
+          if (isLogged) {
+            this.auth.getCurrentUser().then(user => {
+              this.language = this.langList.find(l => l.code === user.lang.code);
+            });
+          } else {
+            this.language = undefined;
+          }
+        });
+      } else {
+        this.language = this.langList.find(l => l.code === this.translate.getBrowserLang());
+      }
+    });
   }
 
-  change(language: string): void {
+  change(language: Lang): void {
     this.language = language;
-    this.translate.use(language);
+    this.translate.use(language.code);
+    this.lang.emit(language);
   }
 
 }
