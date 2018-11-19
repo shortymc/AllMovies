301b28519e7c36bfd7824924f409ffdc98410781
pmb <pm.broca@axess.fr>
2018-11-19 09:54:32 +0100

    move exec in master bat

--- AllMovies.bat
+++ AllMovies.bat
@@ -1,14 +1,16 @@
 @echo off
 cd C:\Users\workspace\git\AllMovies
 :debut
-set /p answer="1. CMD    2. Update    3.Start  "
+set /p answer="1. CMD    2.Code    3. Update    4.Start  "
 set "result=nothing"
 IF /i "%answer%"=="1" (
-	call "AllMovies - CMD.bat"
+	cmd.exe /K "cd ."
 ) else IF /i "%answer%"=="2" (
-	call "AllMovies - Update.bat"
+	code .
 ) else IF /i "%answer%"=="3" (
-	call "AllMovies - Start.bat"
+	call "AllMovies - Update.bat"
+) else IF /i "%answer%"=="4" (
+	call http-serve .\dist
 ) else (
  goto debut
 )
