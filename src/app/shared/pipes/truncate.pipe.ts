48d93c254a6a35d10e2f250f697e2d42ff9b9702
pierre-marie broca <pierre-marie.broca@axess.fr>
2018-11-21 17:57:31 +0100

    reduce size tabs

---
+++ src/app/shared/pipes/truncate.pipe.ts
@@ -0,0 +1,16 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'truncate'
+})
+export class TruncatePipe implements PipeTransform {
+
+  transform(value: string, args: number): any {
+    if (!value || value === '' || value.length < args) {
+      return value;
+    } else {
+      return value.slice(0, args - 1).trim().concat('...');
+    }
+  }
+
+}
