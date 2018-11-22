d072fe12810df0a41d863a9b58149144eaa4fa52
pierre-marie broca <pierre-marie.broca@axess.fr>
2018-11-22 13:55:29 +0100

    add scroll to tabs

--- src/app/shared/components/tabs/tabs.component.ts
+++ src/app/shared/components/tabs/tabs.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { delay } from 'rxjs/operators';
 
 import { TabsService } from './../../service/tabs.service';
 import { TitleService } from '../../service/title.service';
-import { Link } from './../../../model/model';
 
 @Component({
   selector: 'app-tabs',
@@ -16,12 +16,19 @@ export class TabsComponent implements OnInit {
 
   constructor(
     private titleService: TitleService,
-    public tabsService: TabsService
+    public tabsService: TabsService,
+    private elemRef: ElementRef
   ) { }
 
   ngOnInit(): void {
     this.titleService.header.subscribe(title => {
       this.tabsService.updateCurTabLabel(title);
+    });
+    this.tabsService.links.pipe(delay(1000)).subscribe(() => {
+      const active = this.elemRef.nativeElement.querySelector('.mat-tab-label-active');
+      if (active) {
+        active.scrollIntoView(0);
+      }
     });
   }
 
