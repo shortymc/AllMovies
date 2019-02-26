9ac21eda1ac1ed3f17e933b827f8fcb1790797d6
pierre-marie broca <pierre-marie.broca@axess.fr>
2019-02-26 17:46:02 +0100

    routing playing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ const routes: Routes = [
   { path: 'movie', loadChildren: './application-modules/movie-detail/movie-detail.module#MovieDetailModule', canActivate: [AuthGard] },
   { path: 'person', loadChildren: './application-modules/person-detail/person-detail.module#PersonDetailModule', canActivate: [AuthGard] },
   { path: 'release', loadChildren: './application-modules/release/release.module#ReleaseModule', canActivate: [AuthGard] },
+  { path: 'playing', loadChildren: './application-modules/playing/playing.module#PlayingModule', canActivate: [AuthGard] },
   { path: 'movies', loadChildren: './application-modules/movies/movies.module#MoviesModule', canActivate: [AuthGard] },
   { path: 'discover', loadChildren: './application-modules/discover/discover.module#DiscoverModule', canActivate: [AuthGard] },
   { path: 'login', loadChildren: './application-modules/login/login.module#LoginModule' },
