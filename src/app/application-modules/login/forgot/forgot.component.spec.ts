dae15c635a6fccc6fcebbb219b7b0d593ce36e62
pmb <pm.broca@axess.fr>
2019-03-01 15:54:57 +0100

    login module tests

---
+++ src/app/application-modules/login/forgot/forgot.component.spec.ts
@@ -0,0 +1,39 @@
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BrowserModule } from '@angular/platform-browser/';
+import { FormsModule } from '@angular/forms';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+import { SharedModule } from './../../../shared/shared.module';
+import { ForgotComponent } from './forgot.component';
+
+describe('ForgotComponent', () => {
+  let component: ForgotComponent;
+  let fixture: ComponentFixture<ForgotComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ForgotComponent],
+      providers: [TranslateService],
+      imports: [BrowserModule, FormsModule, TranslateModule.forRoot(), HttpClientModule, FontAwesomeModule,
+        RouterTestingModule, SharedModule.forRoot(), MatSnackBarModule, BrowserAnimationsModule, MatFormFieldModule, MatInputModule]
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ForgotComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('init', () => {
+    fixture.detectChanges();
+    expect(component).toBeTruthy();
+  });
+});
