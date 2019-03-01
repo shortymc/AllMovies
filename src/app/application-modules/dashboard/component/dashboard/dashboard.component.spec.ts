f33d597820005785917ae030542845e6183041d5
pmb <pm.broca@axess.fr>
2019-03-01 15:30:35 +0100

    DashboardComponent test

---
+++ src/app/application-modules/dashboard/component/dashboard/dashboard.component.spec.ts
@@ -0,0 +1,35 @@
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BrowserModule } from '@angular/platform-browser/';
+import { FormsModule } from '@angular/forms';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+
+import { SharedModule } from './../../../../shared/shared.module';
+import { DashboardComponent } from './dashboard.component';
+
+describe('DashboardComponent', () => {
+  let component: DashboardComponent;
+  let fixture: ComponentFixture<DashboardComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [DashboardComponent],
+      providers: [TranslateService],
+      imports: [BrowserModule, FormsModule, TranslateModule.forRoot(), HttpClientModule,
+        RouterTestingModule, SharedModule.forRoot(), MatSnackBarModule]
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DashboardComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('init', () => {
+    fixture.detectChanges();
+    expect(component).toBeTruthy();
+  });
+});
