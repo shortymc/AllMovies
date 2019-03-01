ea36fd9fd3376bf3cb797299c95b54d824e5c54e
pierre-marie broca <pierre-marie.broca@axess.fr>
2019-03-01 15:54:57 +0100

    login module tests

---
+++ src/app/application-modules/login/change-question/change-question.component.spec.ts
@@ -0,0 +1,42 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BrowserModule } from '@angular/platform-browser/';
+import { FormsModule } from '@angular/forms';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+import { ChangeQuestionComponent } from './change-question.component';
+import { User } from '../../../model/user';
+import { SharedModule } from '../../../shared/shared.module';
+import { AuthService } from '../../../shared/service/auth.service';
+
+describe('ChangeQuestionComponent', () => {
+  let component: ChangeQuestionComponent;
+  let fixture: ComponentFixture<ChangeQuestionComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ChangeQuestionComponent],
+      providers: [TranslateService],
+      imports: [BrowserModule, FormsModule, TranslateModule.forRoot(), HttpClientModule,
+        RouterTestingModule, SharedModule.forRoot(), MatSnackBarModule, BrowserAnimationsModule, MatFormFieldModule, MatInputModule]
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ChangeQuestionComponent);
+    component = fixture.componentInstance;
+    const auth = TestBed.get(AuthService);
+    spyOn(auth, 'getCurrentUser').and.returnValue(new Promise(resolve => resolve(new User(1, 'name', 'pass', 'q'))));
+  });
+
+  it('init', () => {
+    fixture.detectChanges();
+    expect(component).toBeTruthy();
+  });
+});
