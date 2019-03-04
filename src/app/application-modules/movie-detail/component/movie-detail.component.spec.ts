5b5ca6b997ce1d5933c0edce95b9d6ab43d8488e
pierre-marie broca <pierre-marie.broca@axess.fr>
2019-03-04 10:19:25 +0100

    component test

---
+++ src/app/application-modules/movie-detail/component/movie-detail.component.spec.ts
@@ -0,0 +1,37 @@
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BrowserModule } from '@angular/platform-browser/';
+import { FormsModule } from '@angular/forms';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { RatingModule } from 'ngx-rating';
+
+import { SharedModule } from './../../../shared/shared.module';
+import { MovieDetailComponent } from './movie-detail.component';
+
+describe('MovieDetailComponent', () => {
+  let component: MovieDetailComponent;
+  let fixture: ComponentFixture<MovieDetailComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [MovieDetailComponent],
+      providers: [TranslateService],
+      imports: [BrowserModule, FormsModule, TranslateModule.forRoot(), HttpClientModule, RatingModule,
+        RouterTestingModule, SharedModule.forRoot(), MatSnackBarModule, FontAwesomeModule]
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MovieDetailComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('init', () => {
+    fixture.detectChanges();
+    expect(component).toBeTruthy();
+  });
+});
