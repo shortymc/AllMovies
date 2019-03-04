d90abcce398d23c8bc940d4ce176c420f84e9d39
pierre-marie broca <pierre-marie.broca@axess.fr>
2019-03-04 10:41:51 +0100

    SearchBoxComponent test

---
+++ src/app/application-modules/discover/component/search-box/search-box.component.spec.ts
@@ -0,0 +1,38 @@
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BrowserModule } from '@angular/platform-browser/';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { MatAutocompleteModule, MatFormFieldModule, MatChipsModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { SharedModule } from '../../../../shared/shared.module';
+import { SearchBoxComponent } from './search-box.component';
+
+describe('SearchBoxComponent', () => {
+  let component: SearchBoxComponent<any>;
+  let fixture: ComponentFixture<SearchBoxComponent<any>>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [SearchBoxComponent],
+      providers: [TranslateService],
+      imports: [BrowserModule, FormsModule, ReactiveFormsModule, TranslateModule.forRoot(), HttpClientModule, FontAwesomeModule, MatChipsModule,
+        RouterTestingModule, SharedModule.forRoot(), MatSnackBarModule, MatAutocompleteModule, MatFormFieldModule, BrowserAnimationsModule]
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SearchBoxComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('init', () => {
+    fixture.detectChanges();
+    expect(component).toBeTruthy();
+  });
+});
