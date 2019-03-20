8e6e29bdaa2d99956ced60d744072bd18e40583b
pmb <pm.broca@axess.fr>
2019-03-20 11:43:52 +0100

    search movies for tags

--- src/app/application-modules/tags/tags.module.ts
+++ src/app/application-modules/tags/tags.module.ts
@@ -5,6 +5,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatTableModule } from '@angular/material/table';
 import { LayoutModule } from '@angular/cdk/layout';
+import { MatAutocompleteModule } from '@angular/material';
 import { MatSortModule } from '@angular/material/sort';
 import { MatPaginatorModule, MatPaginatorIntl } from '@angular/material/paginator';
 import { MatInputModule } from '@angular/material/input';
@@ -13,6 +14,7 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
+import { SearchMovieComponent } from './components/search-movie/search-movie.component';
 import { TagsComponent } from './components/tags/tags.component';
 import { SharedModule } from './../../shared/shared.module';
 import { MyPaginator } from '../movies/component/my-paginator';
@@ -40,9 +42,10 @@ const childRoutes: Routes = [
     MatInputModule,
     MatFormFieldModule,
     MatSelectModule,
+    MatAutocompleteModule,
     MatCheckboxModule
   ],
-  declarations: [TagsComponent, TagMoviesComponent],
+  declarations: [TagsComponent, TagMoviesComponent, SearchMovieComponent],
   providers: [
     TranslateService,
     { provide: MatPaginatorIntl, useClass: MyPaginator, deps: [TranslateService] }
