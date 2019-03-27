750dc1645cf3e7f9c79f7f8f1aafc7e11eb5146b
pmb <pm.broca@axess.fr>
2019-03-27 14:00:00 +0100

    lint + test

--- src/app/shared/components/list-tags/list-tags.component.spec.ts
+++ src/app/shared/components/list-tags/list-tags.component.spec.ts
@@ -1,8 +1,24 @@
 /* tslint:disable:no-unused-variable */
+import { MatAutocompleteModule, MatChipsModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TranslateService, TranslateModule } from '@ngx-translate/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
 
+import { SearchTagComponent } from './../search-tag/search-tag.component';
+import { CapitalizeWordPipe } from './../../pipes/capitalizeWord.pipe';
+import { UtilsService } from './../../service/utils.service';
+import { DropboxService } from './../../service/dropbox.service';
+import { ToastService } from './../../service/toast.service';
+import { AuthService } from './../../service/auth.service';
+import { MyMoviesService } from './../../service/my-movies.service';
+import { MyTagsService } from './../../service/my-tags.service';
+import { MenuService } from './../../service/menu.service';
 import { ListTagsComponent } from './list-tags.component';
 
 describe('ListTagsComponent', () => {
@@ -11,9 +27,13 @@ describe('ListTagsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ListTagsComponent ]
+      declarations: [ListTagsComponent, SearchTagComponent],
+      providers: [MenuService, MyTagsService, MyMoviesService, AuthService, TranslateService,
+        ToastService, DropboxService, UtilsService, CapitalizeWordPipe],
+      imports: [ReactiveFormsModule, BrowserAnimationsModule, FormsModule, TranslateModule.forRoot(), HttpClientModule, MatSnackBarModule,
+        RouterTestingModule, FontAwesomeModule, MatAutocompleteModule, MatFormFieldModule, MatChipsModule]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
