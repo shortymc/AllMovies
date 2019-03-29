a44455ce05aab6432baeacf00f0e73eb29bd42ab
pierre-marie broca <pierre-marie.broca@axess.fr>
2019-03-29 11:29:42 +0100

    handling if no score from omdb

--- src/app/shared/service/omdb.service.ts
+++ src/app/shared/service/omdb.service.ts
@@ -13,17 +13,21 @@ export class OmdbService {
     const url = `${Constants.OMDB_URL}${Constants.OMDB_ID}${id}${Constants.OMDB_API_KEY}`;
     return this.serviceUtils.getPromise(url)
       .then((response: any) => {
-        const score = new Score();
-        score.ratings = response.Ratings.map(r => {
-          if (r.Source === 'Internet Movie Database') {
-            r.Source = 'IMDb';
-          }
-          return r;
-        });
-        score.ratings.push({Source: 'Awards', Value: response.Awards});
-        score.imdb_votes = parseInt(response.imdbVotes.replace(/,/g, ''), 10);
-        console.log('score', score);
-        return score;
+        if (!response.Error) {
+          const score = new Score();
+          score.ratings = response.Ratings.map(r => {
+            if (r.Source === 'Internet Movie Database') {
+              r.Source = 'IMDb';
+            }
+            return r;
+          });
+          score.ratings.push({ Source: 'Awards', Value: response.Awards });
+          score.imdb_votes = parseInt(response.imdbVotes.replace(/,/g, ''), 10);
+          console.log('score', score);
+          return score;
+        } else {
+          return undefined;
+        }
       });
   }
 }
