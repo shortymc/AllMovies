ea3c872e393b62a8e47026cc15c03df59f5d2618
pierre-marie broca <pierre-marie.broca@axess.fr>
2019-04-24 10:08:06 +0200

    season detail service

--- src/app/model/season.ts
+++ src/app/model/season.ts
@@ -1,3 +1,18 @@
+export class Episode {
+  id: number;
+  name: string;
+  overview: string;
+  airDate: string;
+  episodeNumber: number;
+  seasonNumber: number;
+  serieId: number;
+  poster: string;
+  guestStars: string[];
+  crew: string[];
+  vote: number;
+  vote_count: number;
+}
+
 export class Season {
   id: number;
   name: string;
@@ -6,4 +21,9 @@ export class Season {
   seasonNumber: number;
   episodeCount: number;
   poster: string;
+  episodes: Episode[];
+  actors: string[];
+  crew: string[];
+  images: string[];
+  videos: string[];
 }
