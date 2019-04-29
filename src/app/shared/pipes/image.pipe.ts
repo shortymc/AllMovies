ddaeee884ca7dba3aa911f2af7c2f12bcf8e128f
pmb <pm.broca@axess.fr>
2019-04-29 12:05:26 +0200

    empty image size

--- src/app/shared/pipes/image.pipe.ts
+++ src/app/shared/pipes/image.pipe.ts
@@ -1,5 +1,4 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { Url } from './../../constant/url';
 
 @Pipe({
   name: 'image'
@@ -8,13 +7,21 @@ export class ImagePipe implements PipeTransform {
   static readonly ORIGINAL_IMG_SIZE = 'original';
   static readonly MEDIUM_IMG_SIZE = 'w154';
   static readonly SMALL_IMG_SIZE = 'w92';
+  static readonly EMPTY_IMAGE_URL = './assets/empty_';
+  static readonly MOVIE_DB_IMAGE_URL = 'https://image.tmdb.org/t/p/';
   static readonly settings = new Map()
     .set('original', ImagePipe.ORIGINAL_IMG_SIZE)
     .set('medium', ImagePipe.MEDIUM_IMG_SIZE)
     .set('small', ImagePipe.SMALL_IMG_SIZE);
 
   transform(path: any, size: 'original' | 'medium' | 'small', noEmpty?: boolean): string {
-    const result = (path === undefined || path === null) ? Url.IMAGE_URL_EMPTY : Url.IMAGE_URL + ImagePipe.settings.get(size) + path;
-    return noEmpty && result === Url.IMAGE_URL_EMPTY ? undefined : result;
+    const width = ImagePipe.settings.get(size);
+    if (path === undefined || path === null && noEmpty) {
+      return undefined;
+    } else if (path === undefined || path === null) {
+      return ImagePipe.EMPTY_IMAGE_URL + width + '.jpg';
+    } else {
+      return ImagePipe.MOVIE_DB_IMAGE_URL + width + path;
+    }
   }
 }
