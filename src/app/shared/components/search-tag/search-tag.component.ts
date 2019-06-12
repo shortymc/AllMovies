f7edb98ba2dd4a9d06850688b6af590c1746bb2c
pmb <pm.broca@atos.net>
2019-06-12 17:41:17 +0200

    keep autocomplete tags open when selection

--- src/app/shared/components/search-tag/search-tag.component.ts
+++ src/app/shared/components/search-tag/search-tag.component.ts
@@ -1,9 +1,9 @@
 import { FormControl } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { switchMap, map, startWith } from 'rxjs/operators';
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
 import { faTimes, faPlus } from '@fortawesome/free-solid-svg-icons';
-import { Sort } from '@angular/material';
+import { Sort, MatAutocomplete, MatAutocompleteTrigger } from '@angular/material';
 
 import { Utils } from './../../utils';
 import { Tag } from './../../../model/tag';
@@ -16,6 +16,7 @@ import { MyTagsService } from '../../service/my-tags.service';
 })
 export class SearchTagComponent implements OnInit {
   @Output() selected = new EventEmitter<Tag>();
+  @ViewChild('inputSearch') inputSearch: ElementRef;
   filteredTags: Observable<Tag[]>;
   tagCtrl: FormControl;
   sort: Sort = { active: 'label', direction: 'asc' };
@@ -44,20 +45,29 @@ export class SearchTagComponent implements OnInit {
         }))));
   }
 
-  add(item: string): void {
+  add(item: string, trigger: MatAutocompleteTrigger, auto: MatAutocomplete): void {
     const tag = new Tag();
     tag.label = item;
     tag.datas = [];
     tag.color = Utils.randomColor();
     this.myTagsService.add(tag).then((added) => {
       this.selected.emit(added);
-      this.tagCtrl.reset();
+      this.resetAutoInput(trigger, auto);
     });
-
   }
 
-  select(item: Tag): void {
+  select(item: Tag, trigger: MatAutocompleteTrigger, auto: MatAutocomplete): void {
     this.selected.emit(item);
-    this.tagCtrl.reset();
+    this.resetAutoInput(trigger, auto);
+  }
+
+  resetAutoInput(trigger: MatAutocompleteTrigger, auto: MatAutocomplete): void {
+    setTimeout(_ => {
+      auto.options.forEach(item => {
+        item.deselect();
+      });
+      this.tagCtrl.reset('');
+      trigger.openPanel();
+    }, 50);
   }
 }
